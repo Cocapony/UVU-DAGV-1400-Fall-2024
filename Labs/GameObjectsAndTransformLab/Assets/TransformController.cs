924175ca346c6608bcfbe9bc38eb3d0fe1c1cf5f
Evan Armstrong <10804001@uvu.edu>
2024-09-12 21:40:44 -0600

    Experimented with Transformations in the Script

--- Labs/GameObjectsAndTransformLab/Assets/TransformController.cs
+++ Labs/GameObjectsAndTransformLab/Assets/TransformController.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 
 public class TransformController : MonoBehaviour
 {
-    private void Update()
+    public float pulseSpeed = 2f;
+    public float maxScale = 1.5f;
+    public float minScale = 0.5f;
+    public GameObject thing;
+
+    void Update()
     {
         // Move the target GameObject
-        var x = Mathf.PingPong(Time.time, 3);
-        var p = new Vector3(0, x, 0);
+        var x = Mathf.PingPong(Time.time, 4);
+        var y = Mathf.PingPong(Time.time, 4);
+        var p = new Vector3(x, y, 0);
         transform.position = p;
-        
+
         // Rotate the target GameObject
-        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+        transform.Rotate(new Vector3(30, 60, 30) * Time.deltaTime);
+
+        //Scale the target GameObject
+        float scale = Mathf.PingPong(Time.time * pulseSpeed, maxScale - minScale) + minScale;
+        transform.localScale = new Vector3(scale, scale, scale);
     }
 }
 
