74722a3c5ca54bbf7baf6a4052d684822913e6d8
Evan Armstrong <10804001@uvu.edu>
2024-09-12 20:30:04 -0600

    Fixed code to work fully with Child3

--- Labs/GameObjectsAndTransformLab/Assets/TransformController.cs
+++ Labs/GameObjectsAndTransformLab/Assets/TransformController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,7 @@ public class TransformController : MonoBehaviour
         // Move the target GameObject
         var x = Mathf.PingPong(Time.time, 3);
         var p = new Vector3(0, x, 0);
+        transform.position = p;
         
         // Rotate the target GameObject
         transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
