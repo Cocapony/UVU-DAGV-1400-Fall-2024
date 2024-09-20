1d858a8f2de1161950e5cf0ab5737b03ecf4e537
Evan Armstrong <10804001@uvu.edu>
2024-09-20 17:20:33 -0600

    Created two 3D objects and added scripts to them

---
+++ Labs/My project/Assets/Scripts/Shrink.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shrink : MonoBehaviour
+{
+    public float shrinkFactor = 0.9f;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Scale the object down when it collides with another object
+        transform.localScale *= shrinkFactor;
+    }
+}
